6d75e67e73c0e0a71b6f620ddd8b4a8eb7d8e790
gchaperon <gabrielchaperonb@gmail.com>
2022-01-09 19:14:47 -0300

    add single_and datamodules to trainig

--- main.py
+++ main.py
@@ -23,22 +23,31 @@ def main(args):
     logging.basicConfig(level=logging.INFO)
 
     print(args)
-    datamodule = cclevr.ExistClevrDataModule(
+    datamodule = cclevr.SingleAndClevrDataModule(
         datadir="data",
         batch_size=args.batch_size,
+        subset_ratio=args.subset_ratio,
     )
     # most params obtained via inspection of dataset
-    model = NSMBaselineLightningModule(
+    nsm_model = NSMLightningModule(
         input_size=45,
         n_node_properties=4,
-        # computation_steps=args.computation_steps,
+        computation_steps=args.computation_steps,
+        encoded_question_size=args.encoded_size,
+        output_size=28,
+        learn_rate=args.learn_rate,
+        # use_instruction_loss=False,
+    )
+    baseline_model = NSMBaselineLightningModule(
+        input_size=45,
+        n_node_properties=4,
         encoded_question_size=args.encoded_size,
         output_size=28,
         learn_rate=args.learn_rate,
         # use_instruction_loss=False,
     )
     metric_to_track = "train_loss"
-    trainer = pl.Trainer(
+    trainer_1 = pl.Trainer(
         gpus=-1 if torch.cuda.is_available() else 0,
         max_epochs=800,
         callbacks=[
@@ -48,7 +57,19 @@ def main(args):
             pl.callbacks.ModelCheckpoint(monitor=metric_to_track),
         ],
     )
-    trainer.fit(model, datamodule)
+    trainer_1.fit(nsm_model, datamodule)
+
+    trainer_2 = pl.Trainer(
+        gpus=-1 if torch.cuda.is_available() else 0,
+        max_epochs=800,
+        callbacks=[
+            pl.callbacks.EarlyStopping(
+                monitor=metric_to_track, patience=50, stopping_threshold=1e-3
+            ),
+            pl.callbacks.ModelCheckpoint(monitor=metric_to_track),
+        ],
+    )
+    trainer_2.fit(baseline_model, datamodule)
 
 
 if __name__ == "__main__":
@@ -57,7 +78,8 @@ if __name__ == "__main__":
 
     parser.add_argument("--batch-size", type=int, required=True)
     parser.add_argument("--learn-rate", type=float, required=True)
-    # parser.add_argument("--computation-steps", type=int, required=True)
+    parser.add_argument("--subset-ratio", type=float, required=True)
+    parser.add_argument("--computation-steps", type=int, required=True)
 
     args = parser.parse_args()
     main(args)
